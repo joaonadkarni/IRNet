7aec2281fd82cb3b13c6d11249829152b4fa4dc7
joaonadkarni <joao.nadkarni@outsystems.com>
2020-07-10 10:56:15 +0100

    add table keys to table data

--- server/utils.py
+++ server/utils.py
@@ -119,7 +119,8 @@ def _data_model_to_spider_data(data_model):
         'db_id': '_'.join(df['entName'].unique().tolist()),
         'primary_keys': df.loc[df['isPrimary'] == True]['index'].tolist(),
         'table_names': [_normalize_word(x) for x in df['entName'].unique().tolist()],
-        'table_names_original': df['entName'].unique().tolist()
+        'table_names_original': df['entName'].unique().tolist(),
+        'table_keys': df['entKey'].unique().tolist()
     }
 
     fks = []
