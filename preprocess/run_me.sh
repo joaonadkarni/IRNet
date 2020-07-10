defcd12e0cac16d4437ddbe9d4d84aa4db92ecec
joaonadkarni <joao.nadkarni@outsystems.com>
2020-07-10 01:35:15 +0100

    remove download nltk from preprocess (only needs to be done once)

--- preprocess/run_me.sh
+++ preprocess/run_me.sh
@@ -6,8 +6,8 @@
 data=$1
 table_data=$2
 
-echo "Start download NLTK data"
-python download_nltk.py
+#echo "Start download NLTK data"
+#python download_nltk.py
 
 echo "Start process the origin Spider dataset"
 python data_process.py --data_path ${data} --table_path ${table_data} --output "process_data.json"
